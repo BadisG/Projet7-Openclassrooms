e1682785d9d86fe7f6210075ad02d1ace095e7b0
BadisG <110173477+BadisG@users.noreply.github.com>
2023-08-30 03:17:00 +0200

    ajout fichier main.py

---
+++ Ghoubali_Badis_1_dashboard_et_API_082023.py
@@ -0,0 +1,11 @@
+import subprocess
+
+# Chemin relatif pour accéder à api.py et dashboard.py
+scripts_directory = "./Ghoubali_Badis_2_dossier_code_082023/Scripts"
+
+# Exécutez api.py avec python
+subprocess.Popen(["python", f"{scripts_directory}/api.py"])
+
+# Exécutez dashboard.py avec streamlit
+subprocess.Popen(["streamlit", "run", f"{scripts_directory}/dashboard.py"])
+
