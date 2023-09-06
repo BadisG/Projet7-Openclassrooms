ce2213f0e05eb6128e8f816ef2e05c654ce5b428
BadisG <110173477+BadisG@users.noreply.github.com>
2023-09-06 05:09:23 +0200

    force le port 5000 pour l'API

--- Ghoubali_Badis_1_dashboard_et_API_082023.py
+++ Ghoubali_Badis_1_dashboard_et_API_082023.py
@@ -1,11 +1,19 @@
 import subprocess
+import os
 
 # Chemin relatif pour accéder à api.py et dashboard.py
 scripts_directory = "./Ghoubali_Badis_2_dossier_code_082023/Scripts"
 
-# Exécutez api.py avec python
-subprocess.Popen(["python", f"{scripts_directory}/api.py"])
+# Obtenez les variables d'environnement actuelles
+env = os.environ.copy()
+
+# Modifiez la variable d'environnement PORT
+env["PORT"] = "5000"
+
+# Exécutez api.py avec python avec la nouvelle variable d'environnement
+subprocess.Popen(["python", f"{scripts_directory}/api.py"], env=env)
 
 # Exécutez dashboard.py avec streamlit
-subprocess.Popen(["streamlit", "run", f"{scripts_directory}/dashboard.py", "--server.port", "8000"])
+subprocess.Popen(["streamlit", "run", f"{scripts_directory}/dashboard.py", "--server.port", "8000"], env=env)
+
 
